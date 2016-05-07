9ce27e02164611f7aeeca1185a9532ff99d92360
sgyurgchyan <suren_gyurgchyan@yahoo.com>
2016-05-06 18:34:18 -0700

    Added files via upload

---
+++ Drop Tables.sql
@@ -0,0 +1,22 @@
+
+-- drop table statements
+drop table address
+
+drop table customer
+
+drop table product
+
+drop table featureType
+
+drop table features
+
+drop table releaseType
+
+drop table release
+
+drop table sourceControl
+
+drop table downloadLink
+
+drop table download
+
